574b3a77b8080709d1773e6d483765e726eec8c5
Csab <107346664+FlyingPaperTiger@users.noreply.github.com>
2023-09-16 15:22:18 +0200

    add game funtion looped 5 times

--- ropasci.js
+++ ropasci.js
@@ -1,26 +1,4 @@
 
-//Assignment exercise 3
-/*
-function getComputerChoise() {
-  //generetes a rundom number between 0 & 2
-  const randomNumber = Math.floor(Math.random() * 3);
-
-  //Maps the number to rock, paper or scissors
-  switch (randomNumber) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissors";
-  }
-}
-
-//console.log(Math.random())
-//console.log(getComputerChoise())
-*/
-//Assignment exercise 4
-
 function playRound () {
   //gets the input of player
   const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
@@ -57,16 +35,35 @@ function playRound () {
   }
 }
 
-const result = playRound();
-console.log(result);
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
 
-/*
-//Assignment exercise 5
-function playRound(playerSelection, computerSelection) {
-    // your code here!
+  for (let round = 1; round <= 5; round++) {
+    console.log(`Round ${round}`);
+    const result = playRound();
+    console.log(result);
+    const scores = `Player: ${playerScore}, Computer:${computerScore}`;
+    console.log(scores);
+
+    if (result.includes("You Win!")) {
+      playerScore++;
+    }
+    else if(result.includes("You Lose!")) {
+      computerScore++;
+    }
   }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-*/
+  console.log("Game Over!");
+
+  if (playerScore > computerScore) {
+    console.log("You win the game!");
+  }
+  else if (playerScore < computerScore) {
+    console.log("You lose the game!");
+  }
+  else {
+    console.log("It´s a tie!");
+  }
+}
+
+game()
